d4920df8d506b2c29ea8fa30d37872f73e2e9391
Alexander Alexy <ax81@airvonalexander.localdomain>
2023-09-23 11:37:06 +0200

    user choice

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const computerChoice = document.getElementById('computer-choice');
-const userChoice = document.getElementById('user-choice');
-const resultDispaly = document.getElementById('result')
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDispaly = document.getElementById('result');
+
+const possibleChoices = document.querySelectorAll('button');
+let userChoice
+
+possibleChoices.forEach(possibleChoice => 
+    possibleChoice.addEventListener('click', (e) => {
+      userChoice = e.target.id;
+      userChoiceDisplay.innerHTML = userChoice;
+    })
+);
