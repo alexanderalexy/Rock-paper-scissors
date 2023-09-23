c21787c2427e08cb33007969d412c68f79938392
Alexander Alexy <ax81@airvonalexander.localdomain>
2023-09-23 11:29:41 +0200

    progress

---
+++ app.js
@@ -0,0 +1,3 @@
+const computerChoice = document.getElementById('computer-choice');
+const userChoice = document.getElementById('user-choice');
+const resultDispaly = document.getElementById('result')
